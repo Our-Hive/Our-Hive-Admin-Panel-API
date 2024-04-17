893ad86dab0e4b0547e38d8894e63a9ec3310a0e
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-17 15:22:47 -0500

    feat(image): implement image data service port (image data use case), add a method to save image

---
+++ internal/domain/api/usecase/ImageDataServiceUseCase.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/model"
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/spi"
+)
+
+type ImageDataServiceUseCase struct {
+	imageStoragePort spi.IImageStoragePort
+}
+
+func NewImageDataServiceUseCase(imageStoragePort spi.IImageStoragePort) *ImageDataServiceUseCase {
+	return &ImageDataServiceUseCase{imageStoragePort: imageStoragePort}
+}
+
+func (i ImageDataServiceUseCase) SaveImageData(imageData *model.ImageData) (imageUrl string, err error) {
+	imageUrl, err = i.imageStoragePort.SaveImage(imageData)
+
+	if err != nil {
+		return "", err
+	}
+
+	return imageUrl, nil
+}
---
+++ internal/domain/api/usecase/ImageUseCase.go
@@ -0,0 +1,24 @@
+package usecase
+
+import (
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/model"
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/spi"
+)
+
+type ImageUseCase struct {
+	imagePersistencePort spi.IImagePersistencePort
+}
+
+func NewImageUseCase(imagePersistencePort spi.IImagePersistencePort) *ImageUseCase {
+	return &ImageUseCase{imagePersistencePort: imagePersistencePort}
+}
+
+func (i ImageUseCase) SaveImage(image *model.Image) error {
+	err := i.imagePersistencePort.SaveImage(image)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
