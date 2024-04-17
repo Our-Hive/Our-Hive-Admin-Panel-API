7ecdb3ec7b3d87072863a89bc0cf42b69002ebdf
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-17 15:21:51 -0500

    fix(image): now a the image storage port returns the url of the image

--- internal/domain/spi/IImageStoragePort.go
+++ internal/domain/spi/IImageStoragePort.go
@@ -3,5 +3,5 @@ package spi
 import "github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/model"
 
 type IImageStoragePort interface {
-	SaveImage(image *model.ImageData) error
+	SaveImage(image *model.ImageData) (fileUrl string, err error)
 }
