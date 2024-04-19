485dd9da5d57745faf7f1ca4b50f12b911a3a240
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-19 17:01:37 -0500

    feat(image-generation): add a controller for image generation and a method for it

---
+++ internal/infrastructure/input/rest/controller/GenerationController.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/application/dto/request"
+	"github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/application/handler"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type GenerationController struct {
+	generationHandler handler.IGenerationHandler
+}
+
+func NewGenerationController(generationHandler handler.IGenerationHandler) *GenerationController {
+	return &GenerationController{generationHandler: generationHandler}
+}
+
+func (g GenerationController) GenerateIAImage(c *gin.Context) {
+	var body request.GenerateIAImage
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	response, httpStatus, err := g.generationHandler.GenerateIAImage(&body)
+
+	if err != nil {
+		c.JSON(httpStatus, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(httpStatus, response)
+}
