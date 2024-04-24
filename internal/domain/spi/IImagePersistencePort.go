f27f82a0028e6e2734c30a4017301f6d66883b5e
Nicolas Olmos <yohannicolas2003@gmail.com>
2024-04-23 22:50:21 -0500

    feat(image): add GetAllImagesFromDatabase method to ImagePersistenceAdapter and port

--- internal/domain/spi/IImagePersistencePort.go
+++ internal/domain/spi/IImagePersistencePort.go
@@ -5,4 +5,5 @@ import "github.com/Our-Hive/Our-Hive-Admin-Panel-API/internal/domain/model"
 type IImagePersistencePort interface {
 	SaveImageInDatabase(image *model.Image) error
 	GetImageFromDatabaseByName(fileName string) (*model.Image, error)
+	GetAllImagesFromDatabase(pageSize int, startAfter string) ([]*model.Image, error)
 }
